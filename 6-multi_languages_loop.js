c037417febee05485030bc21a5c8c8f77a6dcdc4
Uhiara-Chidiadi <uhiarachidiadi@gmail.com>
2025-11-06 11:43:42 +0100

    Detected an error and corrected it

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,14 +1,8 @@
 // This script prints three lines using an array, a loop, and only one console.log call.
 // Constraints: No var, no if/else.
 
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
 
-let output = "";
-let i = 0;
-
-while (i < languages.length) {
-  output += languages[i] + "\n";
-  i++;
+for (const line of lines) {
+  console.log(line);
 }
-
-console.log(output);
