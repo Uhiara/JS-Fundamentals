86282cd9efe18c677022d260d42b59952f605f03
Uhiara-Chidiadi <uhiarachidiadi@gmail.com>
2025-11-06 10:50:59 +0100

    First commit: Add 0-javascript_is_amazing.js

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,7 @@
+// This script creates a constant variable and prints its value.
+
+// Create a constant variable named myVar with the specified string value.
+const myVar = "JavaScript is amazing";
+
+// Print the value of the constant variable to the console.
+console.log(myVar);
